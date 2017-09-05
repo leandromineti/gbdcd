fb6c627927a852bd9dfe9801239ae81d53370ce5
leandro <leandromineti@gmail.com>
2017-09-05 16:17:34 -0300

    Initial commit.

---
+++ R/gaussian.R
@@ -0,0 +1,379 @@
+#' Gaussian BDCD
+#' 
+#' Description
+#'
+#' @author leandromineti@nexer.com.br
+#'
+#' @param y a vector of indices.
+#' @param vix a matrix of neighbors.
+#' @param c parameter indicating the a priori number of clusters.
+#' @param n_iterations number of iterations.
+#' @param burn_in number of discarded iterations.
+#' @param mu0 priori.
+#' @param sigma2 priori.
+#'
+#' @return \code{data.frame} 
+#'
+#' @export
+#' 
+#' @examples
+#'
+#' @family gbdcd
+gaussianBDCD <- function(y, viz, c = 0.35, n_iterations = 1000000, burn_in = 500000,
+                         mu0=0, sigma0=sqrt(2)){
+  
+  require(geoR)
+  require(plyr)
+  
+  ## Padroniza a variável "resposta"
+  mean.y <- mean(y)
+  sd.y   <- sd(y)
+  y      <- (y - mean.y)/sd.y
+  
+  N       <- length(y)
+  Probs   <- (1-c)^(1:N)
+  Probs   <- Probs/sum(Probs)
+  media.k <- round( sum((1:N)*Probs) ) ## Média priori de k.
+  
+  ## Prioris p/ médias
+  sig2ma0   <- sigma0^2   
+  
+  ## Variáveis de armazenamento
+  k_vector       <- rep(NA, n_iterations)
+  v_sigma2       <- rep(NA, n_iterations)
+  v_passos       <- rep(NA, n_iterations)
+  v_centros      <- rep(NA, n_iterations)
+  v_aceite       <- rep(0, n_iterations)
+  mat.Yhat       <- matrix(NA, N, n_iterations)
+  freq_matrix    <- matrix(0, N, N)
+  vec.Medias     <- rep(NA, N)
+  centros        <- sample.int(N, size = media.k, 
+                               replace = FALSE) 
+  particoes      <- RcppPartition(viz, centros)
+  
+  
+  ## Gráfico da informação "a priori" sobre a distribuição do
+  ## número de clusters
+  par(mfrow=c(2,2))
+  barplot(Probs, names.arg = 1:N, col="blue",
+          main="k-Priori")
+  ## Média a priori da distribuição de k.
+  cat("\n Media a priori de k: ", media.k, "\n")
+  
+  ## Configuração inicial do cluster  
+  medias <- aggregate(y ~ particoes, FUN=mean)
+  vec.Medias[medias$particoes] <- medias$y
+  sigma2 <- sum( (y - vec.Medias[particoes])^2 )/(N-length(centros))
+  
+  ## Barra de execução
+  pb <- txtProgressBar(min = 0, max = n_iterations, char= "=", title= "progress bar")
+  
+  ## INICIO DAS ITERAÇÕES
+  for(i in 1:n_iterations)
+  {
+    ## Seleciona o "passo" considerando o estado (tamanho)
+    ## atual dos clusters...
+    k <- length(centros)
+    
+    if(k == N){
+      passo <- sample(c("Death", "Update", "Switch"), size=1, 
+                      prob = c(0.8, 0.1, 0.1))
+    } else { 
+      if (k == 1){
+        passo <- sample(c("Birth", "Update", "Shift"), size=1, 
+                        prob = c(0.8, 0.1, 0.1))
+      } else {
+        passo <- sample(c("Birth", "Death", "Update", "Shift", "Switch"), size=1, 
+                        prob = c(0.35, 0.35, 0.1, 0.1, 0.1))
+      }
+    }
+    
+    ## - - - - - - - - - - - - - - - - - - - - -
+    ## Caso seja selecionado nascimento: "Birth"
+    if(passo == "Birth"){ ## Birth
+      ## Seleciona um potencial novo centro
+      new.centro  <- sample((1:N)[!((1:N) %in% centros)], size=1)
+      ## Insere no vetor de centros
+      new.centros <- append(centros, new.centro, after = sample(0:length(centros), size=1))
+      
+      new.particoes   <- RcppPartition(viz, new.centros)
+      new.medias      <- aggregate(y ~ new.particoes, FUN=mean)
+      new.media       <- subset(new.medias,  new.particoes == new.centro)$y
+      n.prop          <- sum(new.particoes == new.centro)
+      
+      media.proposta  <- (sigma2/(n.prop*sig2ma0 + sigma2))*mu0 + 
+        (n.prop*sig2ma0/(n.prop*sig2ma0 + sigma2))*new.media
+      
+      sigma.proposta  <- sqrt(1/( (1/sig2ma0) +  (n.prop/sigma2)))
+      
+      vec.Medias[new.centro] <- rnorm(1, mean = media.proposta, 
+                                      sd = sigma.proposta)
+      
+      phi <- dnorm(vec.Medias[new.centro], mean = media.proposta, 
+                   sd = sigma.proposta)
+      
+      probMkplus1 <- dnorm(vec.Medias[new.centro], mean = mu0, 
+                           sd = sigma0)
+      
+      ## Cálculo da razão de Verossimilhança
+      LLk      <- sum( dnorm(y, mean = vec.Medias[particoes],     sd=sqrt(sigma2), log=TRUE ) )
+      LLkplus1 <- sum( dnorm(y, mean = vec.Medias[new.particoes], sd=sqrt(sigma2), log=TRUE ) )
+      RAZAO    <- exp(LLkplus1 - LLk)
+      k        <- length(centros)
+      
+      ## A     <- RAZÃO * (1-c) * (1/(N-k)) * 1 * (probMkplus1/phi) ## ORIGINAL
+      A     <- RAZAO * (1-c) * 1 * (probMkplus1/phi)
+      
+      alpha <- min(1, A)
+      
+      if(runif(1) < alpha){ 
+        centros   <- new.centros
+        particoes <- new.particoes
+        v_aceite[i] <- 1
+      }
+    }
+    
+    ## - - - - - - - - - - - - - - - - - - - - -
+    ## Caso seja selecionado morte: "Death"
+    if(passo == "Death"){ ## Death
+      centro  <- sample(centros, 1)
+      medias  <- aggregate(y ~ particoes, FUN=mean)
+      media   <- subset(medias,  particoes == centro)$y
+      n.prop  <- sum(particoes == centro)
+      
+      media.proposta  <- (sigma2/(n.prop*sig2ma0 + sigma2))*mu0 + 
+        (n.prop*sig2ma0/(n.prop*sig2ma0 + sigma2))*media
+      
+      sigma.proposta  <- sqrt(1/( (1/sig2ma0) +  (n.prop/sigma2)))
+      
+      phi     <- dnorm(vec.Medias[centro], mean = media.proposta, 
+                       sd = sigma.proposta) 
+      
+      probMkminus1 <- dnorm(vec.Medias[centro], mean = mu0, 
+                            sd = sigma0) 
+      
+      new.centros   <- centros[-which(centros==centro)] 
+      new.particoes <- RcppPartition(viz, new.centros)
+      new.medias    <- aggregate(y ~ new.particoes, FUN=mean)
+      
+      ## Calculo da razão de Verossimilhança
+      LLk       <- sum( dnorm(y, mean = vec.Medias[particoes],     sd=sqrt(sigma2), log=TRUE ) )
+      LLkminus1 <- sum( dnorm(y, mean = vec.Medias[new.particoes], sd=sqrt(sigma2), log=TRUE ) )
+      RAZAO     <- exp(LLkminus1 - LLk)
+      k         <- length(centros)
+      
+      ## A     <- RAZAO * (1/(1-c)) * (N-k+1) * 1 * (phi/probMkminus1) ## ORIGINAL
+      A     <- RAZAO * (1/(1-c)) * 1 * (phi/probMkminus1)
+      
+      alpha <- min(1, A)
+      
+      if(runif(1) < alpha){ 
+        centros     <- new.centros
+        particoes   <- new.particoes
+        v_aceite[i] <- 1
+      } 
+    }
+    
+    ## - - - - - - - - - - - - - - - - - - - - -
+    ## Caso seja selecionado atualização: "Update"
+    
+    if(passo == "Update"){ ## Update
+      ## Atualiza o vetor de médias
+      medias    <- aggregate(y ~ particoes, FUN=mean)
+      contagens <- as.data.frame(table(particoes))
+      medias    <- merge(medias, contagens, by="particoes") 
+      pesos     <- sigma2/( medias$Freq*sig2ma0 + sigma2 )
+      
+      ## Atualiza VETOR DE MEDIAS: "PRIORI" com "VEROSSIMILHANÇA LOCAL"
+      vec.Medias[medias$particoes] <- rnorm(dim(medias)[1],
+                                            mean = pesos*mu0 + (1-pesos)*medias$y,
+                                            sd   = sqrt( 1/((1/sig2ma0) + (medias$Freq/sigma2)) )  )
+      
+      ## Atualiza o parâmetro de variância POR CONJUGAÇÃO...
+      ## "PRIORI" com "VEROSSIMILHANÇA"
+      ## Supondo médias conhecidas
+      k <- length(centros)
+      
+      if(k < N){
+        soma2  <- sum( (y - vec.Medias[particoes])^2 ) 
+        ## Atualiza o parâmetro de variância POR MÁXIMA VEROSSIMILHANÇA...
+        S2     <- soma2/(N-k)
+        S2.new <- rinvchisq(n=1, df=N-k, scale=S2) ## PROBLEMA COM "df=N-k" quando "N-k -> 0"
+        
+        if(S2.new < 100){
+          sigma2 <- S2.new
+          v_aceite[i] <- 1
+        }
+      }
+      v_aceite[i] <- 1
+    }
+    
+    ## - - - - - - - - - - - - - - - - - - - - -
+    ## Caso seja selecionado morte: "Shift"
+    
+    if(passo == "Shift"){ ## Shift
+      
+      centros_shift_ok  <- centros
+      new.centros       <- centros
+      
+      for(cntr in centros)    
+      {
+        ## Se todos os vizinhos de um centro também são centros: remove o centro do vetor
+        if(all(viz[which(viz[,1]==cntr),2] %in% centros)) 
+          centros_shift_ok <- centros_shift_ok[centros_shift_ok!=cntr]
+      }
+      
+      ## Dentre os centros possíveis, escolhe um
+      if(length(centros_shift_ok)==1){ 
+        centro <- centros_shift_ok
+      } else   centro <- sample(centros_shift_ok, size = 1)
+      ## Vertor com os vizinhos do centro escolhido
+      viz_do_centro <- viz[which(viz[,1]==centro),2]
+      ## Vetor com os vizinhos livres do centro escolhido
+      viz_livres     <- viz_do_centro[!(viz_do_centro %in% centros)]
+      
+      if(length(viz_livres)==1){ 
+        shift <- viz_livres
+      } else   shift <- sample(viz_livres, size = 1)
+      
+      new.centros[which(centros==centro)] <- shift
+      
+      n_Gk <- length(centros_shift_ok)
+      m_Gj <- length(viz_livres)
+      
+      new.particoes  <- RcppPartition(viz, new.centros)
+      
+      vec.Medias[shift] <- vec.Medias[centro]
+      
+      new.centros_shift_ok <- new.centros
+      ## Processo idêntico ao anterior para o novo vetor de centros
+      for(nc in new.centros)   
+      {
+        if(all(viz[which(viz[,1]==nc),2] %in% new.centros)) 
+          new.centros_shift_ok <- new.centros_shift_ok[new.centros_shift_ok!=nc]
+      }
+      
+      ## Vizinhos do novo centro
+      viz_new_centro  <- viz[which(viz[,1]==shift),2]
+      ## Vizinhos livres do novo centro
+      new.viz_livres  <- viz_new_centro[!(viz_new_centro %in% new.centros)]
+      
+      new.n_Gk <- length(new.centros_shift_ok)
+      new.m_Gj <- length(new.viz_livres)
+      
+      ## Cálculo da razão de Verossimilhança
+      LLk       <- sum( dnorm(y, mean = vec.Medias[particoes],     sd=sqrt(sigma2), log=TRUE ) )
+      LLkshift  <- sum( dnorm(y, mean = vec.Medias[new.particoes], sd=sqrt(sigma2), log=TRUE ) )
+      RAZAO     <- exp(LLkshift - LLk)
+      
+      A = RAZAO * (n_Gk/new.n_Gk) * (m_Gj/new.m_Gj)
+      alpha <- min(1, A)
+      
+      if(runif(1) < alpha){
+        centros     <- new.centros
+        particoes   <- new.particoes
+        v_aceite[i] <- 1
+      }
+    }
+    
+    ## - - - - - - - - - - - - - - - - - - - - -
+    ## Caso seja selecionado morte: "Switch"
+    if(passo == "Switch"){ ## Switch
+      
+      ## Escolhe dois centros e troca
+      switch      <- sample(1:length(centros), size=2)
+      new.centros <- replace(centros, list = switch, centros[rev(switch)])
+      
+      new.particoes  <- RcppPartition(viz, new.centros)
+      
+      ## Cálculo da razão de Verossimilhança
+      LLk       <- sum( dnorm(y, mean = vec.Medias[particoes],     sd=sqrt(sigma2), log=TRUE ) )
+      LLkswitch <- sum( dnorm(y, mean = vec.Medias[new.particoes], sd=sqrt(sigma2), log=TRUE ) )
+      RAZAO     <- exp(LLkswitch - LLk)
+      
+      A = RAZAO
+      alpha <- min(1, A)
+      
+      if(runif(1) < alpha){
+        centros     <- new.centros
+        particoes   <- new.particoes
+        v_aceite[i] <- 1
+      }
+      
+    }
+    
+    ## - - - - - - - - - - - - - - - - - - - - -
+    k_vector[i]  <- length(centros)
+    v_sigma2[i]  <- sigma2
+    v_passos[i]  <- passo
+    mat.Yhat[,i] <- vec.Medias[particoes]
+    v_centros[i] <- paste(centros, collapse=";")
+    
+    ## Set progress bar	
+    setTxtProgressBar(pb, i, "running Reversible Jump...")
+    
+    ## Preencher matriz de frequência
+    if(i > burn_in) freq_matrix <- freq_matrix + RcppFreqMatrix(particoes)
+    
+  }
+  ## FIM DAS ITERAÇÕES
+  Sys.sleep(1)
+  close(pb)
+  
+  ## - - - - - - - - - - - - - - - - - - - - - - - 
+  ## Processamento dos resultados
+  
+  ## Retira o Burn-in
+  seq.burn  <- -(1:burn_in)
+  k_vector  <- k_vector[seq.burn]
+  v_sigma2  <- v_sigma2[seq.burn]
+  v_passos  <- v_passos[seq.burn]
+  mat.Yhat  <- mat.Yhat[,seq.burn]
+  v_aceite  <- v_aceite[seq.burn]
+  v_centros <- v_centros[seq.burn]
+  
+  
+  ## Figuras com resultados
+  plot(1:length(k_vector),k_vector,type = 'l', xlab="step", main="k-MCMC")
+  barplot(table(k_vector)/sum(table(k_vector)), col="light blue", main="k-Posteriori") 
+  
+  cat("\n Estimativas p/ k: \n")
+  print( summary(k_vector) )
+  print( quantile(k_vector, probs=c(0.05, 0.95)) )
+  
+  cat("Taxas de aceitação: \n \n")
+  print( aggregate(v_aceite ~ v_passos, FUN=mean) )
+  
+  ## Médias "a posteriori" e "intervalos de credibilidade"
+  Yhat <- apply(mat.Yhat, MARGIN=1, FUN=median)
+  lwr  <- apply(mat.Yhat, MARGIN=1, FUN=function(x) quantile(x, probs=0.05))
+  upr  <- apply(mat.Yhat, MARGIN=1, FUN=function(x) quantile(x, probs=0.95))
+  
+  cat("\n Estimativas p sigma2: \n")
+  print( summary(v_sigma2) )
+  print( quantile(v_sigma2, probs=c(0.05, 0.95)) )
+  
+  ## Processamento da Matrix de Frequências de "conexões"
+  ## Pegando o resultado do Leandro
+  matConexoes <- freq_matrix
+  
+  ## Preenche TODA a matriz
+  matConexoes[lower.tri(matConexoes)] <- t(matConexoes)[lower.tri(matConexoes)]
+  maximo <- max(as.vector(matConexoes), na.rm=TRUE)
+  matConexoes       <- maximo - matConexoes
+  diag(matConexoes) <- maximo + 1
+  
+  ## Forma as partições utilizando análise Hierárquica de clusters
+  clusters <- hclust(as.dist(matConexoes), c("single","complete")[1])
+  plot(clusters, ylab="proximity", main="Proximity Dendogram", xlab="", cex=0.7)
+  
+  saida <- list(mean.info    = cbind(lwr, Yhat, upr),
+                cluster.info = clusters,
+                matConexoes  = matConexoes,
+                k.MCMC       = k_vector,
+                mean.y       = mean.y,
+                sd.y         = sd.y,
+                vec.centros  = v_centros)
+  
+  return(saida)
+}
---
+++ src/rcpparma-source.cpp
@@ -0,0 +1,94 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+
+// we only include RcppArmadillo.h which pulls Rcpp.h in for us
+#include "RcppArmadillo.h"
+
+// via the depends attribute we tell Rcpp to create hooks for
+// RcppArmadillo so that the build process will know what to do
+//
+// [[Rcpp::depends(RcppArmadillo)]]
+
+
+//' Partition of a graph given its centers
+//' 
+//' Greedy partition blah blah blah
+//' @title Graph partition
+//' @param neighbors A matrix defining a the links between the graph nodes.
+//' @param centers The desired centers to start the algorithm
+//' @return a vector indicating the cluster center for each graph node.
+//' @author Marcelo Costa
+// [[Rcpp::export]]
+Rcpp::NumericVector RcppPartition(const arma::mat & neighbors, const Rcpp::NumericVector & centers) {
+  arma::mat viz = neighbors;
+  Rcpp::NumericVector center = centers;
+  int n_vizrows = viz.n_rows;
+  int n_vizcols = viz.n_cols;
+  int n_center  = center.size();
+  int i=0, j=0, k, sum=0, x, a;
+  arma::mat viznew = viz;
+  
+  int n, max;
+  max = viz(i,j);
+  for(i=0;i<n_vizrows;i++){
+    for(j=0;j<n_vizcols;j++)
+      if(viz(i,j)>max) max = viz(i,j);
+  }
+  n = max;
+  
+  Rcpp::NumericVector cluster(n);
+  Rcpp::NumericVector filled(n);
+  
+  for (i=0;i<n;i++){
+    cluster[i] = 0;
+    filled[i] = 0;
+  }
+  
+  while(sum!=n){
+    sum = 0;
+    for(i = 0;i < n_center;i++){
+      x = center[i];
+      cluster[x-1] = x;
+      filled[x-1] = 1;
+      for(j=0;j<n_vizrows;j++){
+        if((viz(j,0)==x) && (filled[viz(j,1)-1] == 0)) {
+          a = viz(j,1);
+          cluster[viz(j,1)-1] = x;
+          filled[viz(j,1)-1]  = 1;
+          for (k=0;k<n_vizrows;k++){
+            if (viz(k,0)==a) {
+              viznew(k,0) = x;
+            }
+          }
+        }
+        
+      }
+    }
+    viz = viznew;
+    for(i=0;i<n;i++){
+      sum = sum + filled[i];
+    }
+  }
+
+  return cluster;
+}
+
+//' Generate frequency matrix
+//' 
+//' Frequency matrix
+//' @title Frequency matrix generation
+//' @param partitions A vector indicating the cluster center for each graph node.
+//' @return a matrix.
+//' @author Leandro Mineti
+// [[Rcpp::export]]
+Rcpp::NumericMatrix RcppFreqMatrix(const Rcpp::NumericVector & partitions) {
+  Rcpp::NumericVector part = partitions;
+  int n = part.size(), mat_index, mat_test;
+  arma::mat mat = arma::zeros(n,n);
+  
+  for(mat_index=0; mat_index<n; mat_index++){
+    for(mat_test=mat_index+1; mat_test<n; mat_test++){
+      if(part[mat_index] == part[mat_test]) mat(mat_index,mat_test) = 1;
+    }
+  }
+  return Rcpp::wrap(mat);
+}
