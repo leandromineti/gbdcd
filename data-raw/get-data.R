0d316eb5df3068580a822c7b593ecd0edf187d92
Leandro Mineti <leandromineti@gmail.com>
2020-09-11 19:59:19 -0300

    Fix check warnings about encodings

--- data-raw/get-data.R
+++ data-raw/get-data.R
@@ -1,4 +1,4 @@
-shape <- rgdal::readOGR("data-raw/", layer = "distribuidoras", encoding = "utf-8")
+shape <- rgdal::readOGR("data-raw/", layer = "distribuidoras", encoding = "latin-1")
 
 df_dea <- read.csv("./data-raw/aneel-407-bias-corrected.csv", encoding = "latin-1")
 df_env <- read.csv("./data-raw/aneel-environmental-data.csv", encoding = "latin-1")
@@ -8,4 +8,4 @@ df_full <- dplyr::full_join(df_dea, df_env, by = c("Codigo" = "d_Code"))
 shape <- merge(shape, df_full, by.x = "Codig", by.y = "Codigo", all.x = FALSE)
 aneelshape <- subset(shape, Codig != "D05")
 
-usethis::use_data(aneelshape, overwrite = TRUE)
+usethis::use_data(aneelshape, overwrite = FALSE, version = 3)
